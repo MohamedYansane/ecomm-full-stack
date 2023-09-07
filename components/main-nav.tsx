4ab5052ac8a08f38ad140ca24aeec150fc0feadb
Mohamed Yansane <yansane472@gmail.com>
2023-09-07 22:33:05 +0000

    adding product to the navbar

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,10 +13,38 @@ export const MainNav = ({
   const params = useParams();
   const routes = [
     {
-      href: `/${params.storeId}/settings`,
-      label: "Settings",
-      active: pathname === `/${params.storeId}/settings`,
+      href: `/${params.storeId}`,
+      //instead of Dashboard i can call it Overview it doesnt matter
+      label: "Dashboard",
+      active: pathname === `/${params.storeId}`,
     },
+    {
+      href: `/${params.storeId}/billboards`,
+      label: "Billboards",
+      active: pathname === `/${params.storeId}/billboards`,
+    },
+    {
+      href: `/${params.storeId}/categories`,
+      label: "Categories",
+      active: pathname === `/${params.storeId}/categories`,
+    },
+    {
+      href: `/${params.storeId}/colors`,
+      label: "Colors",
+      active: pathname === `/${params.storeId}/colors`,
+    },
+    {
+      href: `/${params.storeId}/products`,
+      label: "Products",
+      active: pathname === `/${params.storeId}/products`,
+    },
+
+    {
+      href: `/${params.storeId}/sizes`,
+      label: "Sizes",
+      active: pathname === `/${params.storeId}/sizes`,
+    },
+
     {
       href: `/${params.storeId}/settings`,
       label: "Settings",
