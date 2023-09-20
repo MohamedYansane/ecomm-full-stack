46ae1aa41c4e25268a702fdb09bab6088ee8edae
Mohamed Yansane <yansane472@gmail.com>
2023-09-20 23:55:09 +0000

    creating my webhook to test my stripe local

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,74 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(req: Request) {
+  //we r not using req.json here cause this webhook
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+  const address = session?.customer_details?.address;
+
+  // after clicking checkout in our stripe page we r seeing adress and city
+  // so i want to create using one component or object addressComponents
+  const addressComponents = [
+    address?.line1,
+    address?.line2,
+    address?.city,
+    address?.state,
+    address?.postal_code,
+    address?.country,
+  ];
+
+  const addressString = addressComponents.filter((c) => c !== null).join(", ");
+  //the event we want to listen here checkout.session.completed
+  if (event.type === "checkout.session.completed") {
+    const order = await prismadb.order.update({
+      where: {
+        //tring to access our metadata
+        id: session?.metadata?.orderId,
+      },
+      data: {
+        isPaid: true,
+        address: addressString,
+        phone: session?.customer_details?.phone || "",
+      },
+      include: {
+        orderItems: true,
+      },
+    });
+
+    const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+    // then we've to archived our products
+
+    await prismadb.product.updateMany({
+      where: {
+        id: {
+          in: [...productIds],
+        },
+      },
+      data: {
+        isArchived: true,
+      },
+    });
+  }
+
+  return new NextResponse(null, { status: 200 });
+}
